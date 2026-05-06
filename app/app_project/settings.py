414adb59c0352b69df71205015fd8a7b09fbad1b
NLA73 <nathanaella123@gmail.com>
2026-05-06 22:06:05 +0700

    Update settings.py

--- app/app_project/settings.py
+++ app/app_project/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # 2. Keamanan (Isi random aja kalau buat belajar)
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ['*'] # Biarkan sementara agar mudah akses
 CSRF_TRUSTED_ORIGINS = [
     'https://*.ngrok-free.app',
